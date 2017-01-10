715e0558879f11888d2aa2491cabb63b933c631f
zhaojie <582863656@qq>
2017-01-10 15:18:11 +0800

    1.10 change

---
+++ app/src/main/java/com/bupt/bignews/entity/MessageEvent.java
@@ -0,0 +1,21 @@
+package com.bupt.bignews.entity;
+
+import android.view.MotionEvent;
+
+/**
+ * Created by ZhaoJie1234 on 2017/1/10.
+ */
+public class MessageEvent {
+    private ListNewsBean listNewsBean;
+    public MessageEvent(ListNewsBean listNewsBean){
+        this.listNewsBean = listNewsBean;
+    }
+
+    public ListNewsBean getListNewsBean() {
+        return listNewsBean;
+    }
+
+    public void setListNewsBean(ListNewsBean listNewsBean) {
+        this.listNewsBean = listNewsBean;
+    }
+}
